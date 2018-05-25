ac04b598614409daeffeea67186aacc8dd274bba
chava33 <ssenthu@yahoo.ca>
2018-05-25 19:32:35 -0400

    All in virtual box

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ sudo groupadd --system ggc_group
 sudo apt-get update
 sudo apt-get install -y sqlite3 python2.7 binutils curl
 
+
 wget -O /vagrant/downloads/root.ca.pem http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
 
 # Copy greengrass binaries
@@ -16,10 +17,9 @@ sudo apt-get install -y python-pip python-dev gcc
 sudo pip install mxnet
 sudo apt-get install -y python-opencv
 
-# Copy certificates and configurations
-sudo cp /vagrant/certs/* /greengrass/certs
-sudo cp /vagrant/config/* /greengrass/config
-sudo cp /vagrant/downloads/root.ca.pem /greengrass/certs
+# Install aws cli
+sudo pip install awscli
+sudo pip install greengo
 
 # Create a source for images to map into the Lambda space later
 sudo mkdir /images
@@ -28,5 +28,17 @@ sudo chown ggc_user:ggc_group /images
 # Back up group.json - you'll thank me later
 sudo cp /greengrass/ggc/deployment/group/group.json /greengrass/ggc/deployment/group/group.json.orig
 
-cd /greengrass/ggc/core
-sudo ./greengrassd start
+
+git clone https://github.com/dzimine/aws-greengrass-ml-inference.git
+
+#manually run in virtual machine
+# AWS CLI
+#aws configure
+
+#cd  /home/vagrant/aws-greengrass-ml-infere
+#greengo create
+#sudo cp /home/vagrant/aws-greengrass-ml-infere/certs/* /greengrass/certs
+#sudo cp /home/vagrant/aws-greengrass-ml-infere/config/* /greengrass/config
+#sudo cp /vagrant/downloads/root.ca.pem /greengrass/certs
+#cd /greengrass/ggc/core
+#sudo ./greengrassd start
